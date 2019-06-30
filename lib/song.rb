5976341b56d83e5360d201ea2af426d57759cc7c
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-30 21:37:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
     song = self.new
     split_filename = filename.split("-")
     song.artist_name = split_filename[0]
-    (".mp3").split("-")
+    song_name(".mp3").split("-")
     song.name = new_filename[1]
     song.artist_name = new_filename[0]
     song.name 
